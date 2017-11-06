292bd5b0c55fb4c2cec59c8d0f02bf2e476c1ca7
Radoslaw Mejer <radmen@desmart.com>
2017-11-06 11:00:23 +0100

    Fix: entrypoint fails when pipefail is set

--- entrypoint.sh
+++ entrypoint.sh
@@ -3,13 +3,13 @@
 
 declare -r ENV_FILE=/etc/profile.d/torus.sh
 
-if torus ls > /dev/null; then
-    torus view -o $TORUS_ORG -p $TORUS_PROJECT -e $TORUS_ENV -s $TORUS_SERVICE | sed -e 's/^/export /' >> $ENV_FILE
-    
-    if grep -q -i "Incorrect usage\|Missing flags" $ENV_FILE; then
-        echo "" > $ENV_FILE
-    fi
+__torus_export_env ()
+{
+    torus view -o $TORUS_ORG -p $TORUS_PROJECT -e $TORUS_ENV -s $TORUS_SERVICE || echo ""
+}
 
+if torus ls > /dev/null; then
+    __torus_export_env | sed '/^\s*$/d' | sed -e 's/^/export /' >> $ENV_FILE
     . $ENV_FILE
 fi
 
